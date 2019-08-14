b76198b97741bb32fa35f944cad1024f2a8c7a28
Lisonallie <allisonvanlinden@gmail.com>
2019-08-14 13:37:29 +0200

    remove wrong code, start over

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
-const validator = require("email-validator");
 const axios = require('axios');
 // validator.validate("test@email.com"); // true
 console.log("Hello, Node.JS!");
-let input = document.createElement('input');
 
-window.onload() = input;
-
-async function translate() {
-    let phrase = input.value;
-    
-    let api = `https://api.mymemory.translated.net/get?q=${phrase}&langpair=en|it`
+function translate() {
+  
+    let api = `https://api.mymemory.translated.net/get?q=${phrase}&langpair=${fromLang}|${toLang}`
     const response = await axios.get(api);
     console.log(response);
-}      
+    phrase.innerHTML = response;
+}
+
+executeTranslation.addEventListener("click", translate);
 
