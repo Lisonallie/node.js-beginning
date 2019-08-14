96f3acb054bf038f7cae7a14f7b82293f8fd439b
Lisonallie <allisonvanlinden@gmail.com>
2019-08-14 14:25:50 +0200

    add axios function syntax

--- index.js
+++ index.js
@@ -8,33 +8,15 @@ let standardInput = process.stdin;
 standardInput.setEncoding('utf-8');
 
 standardInput.on('data', function (data) {
-
-    let phrase = "It's a beautiful day.";
     let fromLang = "";
     let toLang = "fr";
-    let api = `https://api.mymemory.translated.net/get?q=${phrase}&langpair=${fromLang}|${toLang}`;
 
-    // Make a request for a user with a given ID
-    axios.get(api)
-        .then(function (response) {
-            // handle success
-            console.log(response);
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .finally(function () {
-            // always executed
-        });
-    // User input exit.
-    if (data === 'exit\n') {
-        // Program exit.
-        console.log("User input complete, program exit.");
-        process.exit();
-    } else {
-        // Print user input in console.
-        console.log('User Input Data : ' + data);
-    }
+    axios({
+        method: "get",
+        url: `https://api.mymemory.translated.net/get?q=${data}&langpair=${fromLang}|${toLang}`,
+        responseType: "json"
+    }).then(function (response) {
+        console.log(response.data);
+    });
 });
 
