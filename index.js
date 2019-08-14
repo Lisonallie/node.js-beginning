27dbeb8196183d538064117008d15d1804c1f108
Lisonallie <allisonvanlinden@gmail.com>
2019-08-14 11:56:43 +0200

    add beginning axios function

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const validator = require("email-validator");
 const axios = require('axios');
 // validator.validate("test@email.com"); // true
 console.log("Hello, Node.JS!");
+let input = document.createElement('input');
+let phrase = input.value.length;
+
+window.onload() = input;
+
+async function translate() {
+    let key = ""
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+}      
+
